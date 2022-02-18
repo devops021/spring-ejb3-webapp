f50c057c1f0d83d1042f839d402fe6cb75effe01
devops021 <80098577+devops021@users.noreply.github.com>
2022-02-18 17:03:12 +0530

    Update BlogPostController.java

--- src/main/java/com/github/bpark/BlogPostController.java
+++ src/main/java/com/github/bpark/BlogPostController.java
@@ -1,20 +1,4 @@
-/*
- * This file is part of websocktets-gl - simple WebSocket example
- * Copyright (C) 2012  Burt Parkers
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+
 
 package com.github.bpark;
 
@@ -36,17 +20,6 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.util.List;
 
-/**
- * Spring controller to handle requests from the gui.
- *
- * Note: Two stateless session beans are used in two ways. One with @EJB injection, one with spring DI.
- * <p/>
- * <h3>Extra-Info</h3>
- * Created: 19:23 05.06.12
- *
- * @author Burt Parkers
- * @version 1.0
- */
 @Controller
 @RequestMapping(value="/blogposts")
 public class BlogPostController {
