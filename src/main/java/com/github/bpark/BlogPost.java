ab3dec43c22c703d2a937c56ed12d9b4de0149bb
devops021 <80098577+devops021@users.noreply.github.com>
2022-02-18 17:02:23 +0530

    Update BlogPost.java

--- src/main/java/com/github/bpark/BlogPost.java
+++ src/main/java/com/github/bpark/BlogPost.java
@@ -1,35 +1,6 @@
-/*
- * This file is part of websocktets-gl - simple WebSocket example
- * Copyright (C) 2012  Burt Parkers
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
 
 package com.github.bpark;
 
-/**
- * Entity class for blog posts.
- *
- * Note not all fields are used for this example application. The field shortcontent is used to prevent loading the full
- * article content by findAll() Methods.
- * <p/>
- * <h3>Extra-Info</h3>
- * Created: 19:13 05.06.12
- *
- * @author Burt Parkers
- * @version 1.0
- */
 
 import javax.persistence.Basic;
 import javax.persistence.Entity;
